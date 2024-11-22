06ef5c9db2b4c45fb1d302cce4fae3eab1bcd18f
Utkarsh <142315293+utkarsh10000@users.noreply.github.com>
2024-11-22 21:53:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://nestable-1.onrender.com',
-        secure: false,
-      },
-    },
-  },
-
-  plugins: [react()],
-});
